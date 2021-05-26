0046206523428cc07afb1edd4f563da15374bb58
aguizzo <84861594+aguizzo@users.noreply.github.com>
2021-05-26 18:20:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,22 @@
-# pylint: enable=missing-docstring
-
+""" Comment """
 import unittest
 import transform
 
 
+""" Comment """
 class TestStringMethods(unittest.TestCase):
 
+""" Comment """
     def test_is_upper(self):
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
 
+""" Comment """
     def test_is_lower(self):
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
+""" Comment """
     def test_is_capitalize(self):
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")

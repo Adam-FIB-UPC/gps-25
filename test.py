e97bb9f216afef26b180b3555e196059bb750b91
aguizzo <84861594+aguizzo@users.noreply.github.com>
2021-05-26 18:24:20 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,17 @@
-""" Comment """
 import unittest
 import transform
 
 
-""" Comment """
 class TestStringMethods(unittest.TestCase):
 
-""" Comment """
     def test_is_upper(self):
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
 
-""" Comment """
     def test_is_lower(self):
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
 
-""" Comment """
     def test_is_capitalize(self):
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
